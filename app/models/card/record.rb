81e4ab42d80db64c6e2b8072f4bffa593eae4890
nicolaskelepuris <nicolaskelepuris@gmail.com>
2023-02-01 14:37:14 -0300

    annotate models

--- app/models/card/record.rb
+++ app/models/card/record.rb
@@ -8,7 +8,6 @@
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  stackable  :boolean          default(FALSE)
 #  url        :string
 #  type       :string           not null
 #  value      :integer          default(0), not null
--- app/models/card/stackable_weapon.rb
+++ app/models/card/stackable_weapon.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: cards
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  url        :string
+#  type       :string           not null
+#  value      :integer          default(0), not null
+#
 module Card
   class StackableWeapon < Record
   end
