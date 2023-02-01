48a9b0ba1e440f45ca4907f9e1a17a7a04ed0eca
nicolaskelepuris <nicolaskelepuris@gmail.com>
2023-02-01 12:05:35 -0300

    create serializer for card

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,25 +2,6 @@
 
 class CardsController < ApplicationController
   def index
-    render json: { data: ::Card::List.call.map { |c| format(c) } }
-  end
-
-  private
-
-  def format(card)
-    card.as_json(only: %i[id name value]).merge(type: format_type(card.type))
-  end
-
-  def format_type(type)
-    case type
-    when Card::Weapon.to_s
-      'weapon'
-    when Card::Armor.to_s
-      'armor'
-    when Card::HealthPotion.to_s
-      'health potion'
-    else
-      'N/A'
-    end
+    render json: { data: ::Card::List.call.map { |c| ::Card::Serializer.call(c) } }
   end
 end
---
+++ app/models/card/serializer.rb
@@ -0,0 +1,24 @@
+module Card
+  module Serializer
+    extend self
+
+    def call(card)
+      card.as_json(only: %i[id name value]).merge(type: format_type(card.type))
+    end
+
+    private
+
+    def format_type(type)
+      case type
+      when Card::Weapon.to_s
+        'weapon'
+      when Card::Armor.to_s
+        'armor'
+      when Card::HealthPotion.to_s
+        'health potion'
+      else
+        'N/A'
+      end
+    end
+  end
+end
