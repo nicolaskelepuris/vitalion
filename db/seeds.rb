564ee8d12c61dcecdbc4fa43a8fe5069c8d0a005
nicolaskelepuris <nicolaskelepuris@gmail.com>
2023-02-01 10:36:59 -0300

    update db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,21 @@
 # frozen_string_literal: true
 
 if Card::Record.count.zero?
-  Card::Record.create(name: 'card 1', attack: 2, defense: 0, stackable: true)
-  Card::Record.create(name: 'card 2', attack: 3, defense: 0, stackable: true)
-  Card::Record.create(name: 'card 3', attack: 4, defense: 0, stackable: true)
-  Card::Record.create(name: 'card 5', attack: 5, defense: 0)
-  Card::Record.create(name: 'card 6', attack: 8, defense: 0)
-  Card::Record.create(name: 'card 7', attack: 10, defense: 0)
-  Card::Record.create(name: 'card 8', attack: 18, defense: 0)
+  Card::Weapon.create(name: 'weapon 1', value: 2, stackable: true)
+  Card::Weapon.create(name: 'weapon 2', value: 3, stackable: true)
+  Card::Weapon.create(name: 'weapon 3', value: 4, stackable: true)
+  Card::Weapon.create(name: 'weapon 5', value: 5)
+  Card::Weapon.create(name: 'weapon 6', value: 8)
+  Card::Weapon.create(name: 'weapon 7', value: 10)
+  Card::Weapon.create(name: 'weapon 8', value: 18)
 
-  Card::Record.create(name: 'card 9', attack: 0, defense: 3)
-  Card::Record.create(name: 'card 10', attack: 0, defense: 6)
-  Card::Record.create(name: 'card 11', attack: 0, defense: 7)
-  Card::Record.create(name: 'card 12', attack: 0, defense: 9)
-  Card::Record.create(name: 'card 13', attack: 0, defense: 12)
+  Card::Armor.create(name: 'armor 1', value: 3)
+  Card::Armor.create(name: 'armor 2', value: 6)
+  Card::Armor.create(name: 'armor 3', value: 7)
+  Card::Armor.create(name: 'armor 4', value: 9)
+  Card::Armor.create(name: 'armor 5', value: 12)
+
+  Card::HealthPotion.create(name: 'health potion 1', value: 5)
+  Card::HealthPotion.create(name: 'health potion 2', value: 10)
+  Card::HealthPotion.create(name: 'health potion 3', value: 15)
 end
