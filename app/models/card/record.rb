2be8db8cda08413d0abe82a84a57bc56b8de351f
nicolaskelepuris <nicolaskelepuris@gmail.com>
2023-02-01 10:12:56 -0300

    add and use annotate gem

--- app/models/card/record.rb
+++ app/models/card/record.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: cards
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  attack     :integer
+#  defense    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  stackable  :boolean          default(FALSE)
+#  url        :string
+#
 module Card
   class Record < ApplicationRecord
     self.table_name = 'cards'
